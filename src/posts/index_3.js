ab758f955bd41fdb015892ca7964da07130ecd32
littleroutes <littleroutes.co@gmail.com>
2026-06-28 23:11:04 +0200

    Add files via upload

---
+++ src/posts/index_3.js
@@ -0,0 +1,23 @@
+import { post as colosseum } from "./what-to-tell-kids-at-the-colosseum.js";
+import { post as kyotoToddler } from "./kyoto-with-toddler-tips.js";
+import { post as parisTeenagers } from "./paris-with-teenagers-tips.js";
+import { post as fushimiInari } from "./fushimi-inari-with-kids.js";
+import { post as romeToddlers } from "./rome-with-toddlers.js";
+import { post as louvre } from "./what-to-tell-kids-at-the-louvre.js";
+import { post as romeTeenagers } from "./rome-with-teenagers-tips.js";
+import { post as arashiyama } from "./what-to-say-at-arashiyama-bamboo-grove.js";
+import { post as notreDame } from "./notre-dame-rebuild-what-to-tell-kids.js";
+import { post as gion } from "./gion-with-kids-tips.js";
+
+export const posts = [
+  colosseum,
+  kyotoToddler,
+  parisTeenagers,
+  fushimiInari,
+  romeToddlers,
+  louvre,
+  romeTeenagers,
+  arashiyama,
+  notreDame,
+  gion,
+];
