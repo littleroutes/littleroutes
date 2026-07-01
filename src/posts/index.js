18913429f1707bcdd8eda81156d59f181b27e75d
littleroutes <littleroutes.co@gmail.com>
2026-07-01 23:46:26 +0200

    Update index.js

--- src/posts/index.js
+++ src/posts/index.js
@@ -1,23 +1,23 @@
-import { post as colosseum } from "./what-to-tell-kids-at-the-colosseum.js";
-import { post as kyotoToddler } from "./kyoto-with-toddler-tips.js";
-import { post as parisTeenagers } from "./paris-with-teenagers-tips.js";
-import { post as fushimiInari } from "./fushimi-inari-with-kids.js";
-import { post as romeToddlers } from "./rome-with-toddlers.js";
-import { post as louvre } from "./what-to-tell-kids-at-the-louvre.js";
-import { post as romeTeenagers } from "./rome-with-teenagers-tips.js";
 import { post as arashiyama } from "./what-to-say-at-arashiyama-bamboo-grove.js";
 import { post as notreDame } from "./notre-dame-rebuild-what-to-tell-kids.js";
 import { post as gion } from "./gion-with-kids-tips.js";
+import { post as louvre } from "./what-to-tell-kids-at-the-louvre.js";
+import { post as romeTeenagers } from "./rome-with-teenagers-tips.js";
+import { post as eiffelTower } from "./what-to-tell-kids-eiffel-tower.js";
+import { post as romeUnder6 } from "./rome-with-kids-under-6.js";
+import { post as colosseum } from "./what-to-tell-kids-at-the-colosseum.js";
+import { post as kyotoFacts } from "./kyoto-facts-for-kids.js";
+import { post as kyotoToddler } from "./kyoto-with-toddler-tips.js";
 
 export const posts = [
-  colosseum,
-  kyotoToddler,
-  parisTeenagers,
-  fushimiInari,
-  romeToddlers,
-  louvre,
-  romeTeenagers,
   arashiyama,
   notreDame,
   gion,
+  louvre,
+  romeTeenagers,
+  eiffelTower,
+  romeUnder6,
+  colosseum,
+  kyotoFacts,
+  kyotoToddler,
 ];
